079c7ee764a1004fcc03bd828e5071dab58437ab
gubinvs@gmail.com <gubinvs@gmail.com>
2025-10-06 14:08:30 +0300

    w

--- src/index.php
+++ src/index.php
@@ -7,25 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="0;url=https://encomponent.ru/snab.php">
     <link rel="icon" href="https://encomponent.ru/favicon.svg" type="image/svg+xml">
-    <!-- <link rel="stylesheet" href="css/encomp-nku-project-style.css" media="all"> -->
-    <!-- <meta name="description" content="Производство автоматизированных систем для реализации проектов в реальных секторах экономики: 
-                                    промышленных производствах, энергетике, машиностроении и других отраслях обеспечивающих материальное производство."> -->
-    <!-- <title>Компоненты энергии - Производство автоматизированных систем, материальное и информационное обеспечение</title> -->
+    <link rel="stylesheet" href="css/encomp-nku-project-style.css" media="all">
+    <meta name="description" content="Производство автоматизированных систем для реализации проектов в реальных секторах экономики: 
+                                    промышленных производствах, энергетике, машиностроении и других отраслях обеспечивающих материальное производство.">
+    <title>Компоненты энергии - Производство автоматизированных систем, материальное и информационное обеспечение</title>
 </head>
 
 <body>
     <!--Подключение header-->
-    <!-- <?php include "php/modules/header.php" ?> -->
+    <?php include "php/modules/header.php" ?>
     <!--Основная секция страницы-->
-    <!-- <main>
+    <main>
         <?php include "php/modules/main_index_page_section.php" ?>
         <?php include "php/modules/index_solve-problems_section.php" ?>
         <?php include "php/modules/index_case_section.php" ?>
-    </main> -->
+    </main>
     <!--Подключение header-->
-    <!-- <?php include "php/modules/footer.php" ?> -->
+    <?php include "php/modules/footer.php" ?>
     <!--Подключение скриптов JS-->
-    <!-- <script src="js/app.js" type="module"></script> -->
+    <script src="js/app.js" type="module"></script>
 </body>
 
 </html>

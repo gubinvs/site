b15601f18eef92fbed119bd4e39a0e5effb53243
gubinvs@gmail.com <gubinvs@gmail.com>
2025-10-06 13:54:16 +0300

    в

--- src/php/modules/footer.php
+++ src/php/modules/footer.php
@@ -19,6 +19,7 @@ header("Access-Control-Allow-Headers: Content-Type");
                     <!-- <div class="footer-nav__item"><a href="https://encomponent.ru/production.php" class="footer-nav__link">Сборка щитов автоматики</a></div> -->
                     <div class="footer-nav__item"><a href="https://encomponent.ru/snab.php" class="footer-nav__link">Аутсорсинг рутины снабжения</a></div>
                     <div class="footer-nav__item"><a href="https://shop.encomponent.ru/" class="footer-nav__link">Интернет-магазин комплектующих</a></div>
+                    <div class="footer-nav__item"><a href="https://supply.encomponent.ru/" class="footer-nav__link">Сервис для снабженца</a></div>
                     <!-- <div class="footer-nav__item"><a href="https://encomponent.ru/about.php" class="footer-nav__link">Центр раскрытия корпоративной информации</a></div> -->
                     <div class="footer-nav__item"><a href="https://encomponent.ru/pp-page.html" class="footer-nav__link">Политика обработки персональных данных</a></div>
                 </ul>

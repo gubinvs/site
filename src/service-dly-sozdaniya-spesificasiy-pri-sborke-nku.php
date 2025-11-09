a3af4f28949b0260002492e24c3553fb691263a2
master <gubinvs@gmail.com>
2025-11-09 12:36:25 +0300

    s

---
+++ src/service-dly-sozdaniya-spesificasiy-pri-sborke-nku.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://encomponent.ru/favicon.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="../css/supply-index-page-style.css" media="all">
+
+    <!-- 🔹 Базовое SEO -->
+    <title><?php echo $titlePage ?></title>
+    <meta name="description" content="Запустили в работу сервис для создания спецификаций с актуальными ценами поставщиков, в том числе и санкционных комплектующих. Разработчик группак омпаний Компоненты энергии. Сервис условно бесплатный, так и с ежемесячной подпиской. Имеется возможность установки персонально на собственный сервер для локализации данных. Упрощает процесс создания спецификаций о определения себестоимости готового изделия.">
+    <meta name="keywords" content="Сервис для создания спецификаций с актуальными ценами поставщиков, в том числе и санкционных комплектующих">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="https://encomponent.ru/service-dly-sozdaniya-spesificasiy-pri-sborke-nku.php">
+
+    <!-- 🔹 Open Graph для соцсетей -->
+    <meta property="og:type" content="product">
+    <meta property="og:title" content="Сервис для создания спецификаций">
+    <meta property="og:description" content="Запустили в работу сервис для создания спецификаций с актуальными ценами поставщиков, в том числе и санкционных комплектующих. Разработчик группак омпаний Компоненты энергии. Сервис условно бесплатный, так и с ежемесячной подпиской. Имеется возможность установки персонально на собственный сервер для локализации данных. Упрощает процесс создания спецификаций о определения себестоимости готового изделия.">
+    <meta property="og:image" content="https://encomponent.ru/img-email/specification-nku.jpg">
+    <meta property="og:url" content="https://encomponent.ru/service-dly-sozdaniya-spesificasiy-pri-sborke-nku.php">
+    <meta property="og:site_name" content="Компоненты энергии">
+
+    <!-- 🔹 Schema.org — структурированные данные -->
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org/",
+            "@type": "Product",
+            "name": "Сервис для создания спецификаций",
+            "image": "https://encomponent.ru/img-email/specification-nku.jpg",
+            "sku": "supply_encomponent_ru",
+            "brand": {
+                "@type": "Brand",
+                "name": "Компоненты энергии"
+            },
+            "offers": {
+                "@type": "Offer",
+                "priceCurrency": "RUB",
+                "price": "Условно бесплатно",
+                "availability": " "
+                Много " ? 'https://schema.org/InStock' : 'https://schema.org/OutOfStock' ?>",
+                "url": "https://encomponent.ru/service-dly-sozdaniya-spesificasiy-pri-sborke-nku.php"
+            },
+            "description": "Сервис для создания спецификаций с актуальными ценами поставщиков, в том числе и санкционных комплектующих"
+        }
+    </script>
+
+    <!-- 🔹 Микроданные для Яндекса -->
+    <meta name="yandex-verification" content="26c39d63e5887901" />
+</head>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) {
+                return;
+            }
+        }
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(98501628, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true
+    });
+</script>
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/98501628" style="position:absolute; left:-9999px;" alt="s" /></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
+
+<body>
+    <main>
+        <div class="container">
+            <div class="card">
+                <div class="card__number-block">
+                    <div class="c-number-block__triangle">
+                        <svg width="22" height="15" viewBox="0 0 22 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.8253 15L0 0L21.6506 0L10.8253 15Z" fill="#8E9934" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>

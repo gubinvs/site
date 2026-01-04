dd53ff320e0a5350fb9c5813b9ce1ef2f048f889
master <gubinvs@gmail.com>
2026-01-04 23:43:40 +0300

    w

--- src/php/modules/directory-groups-index-page.php
+++ src/php/modules/directory-groups-index-page.php
@@ -1,30 +1,30 @@
-<div className="directory-groups__container">
-    <h2 className="directory-groups__title"> <span class="yellow-diamond"></span> Категории интернет-магазина</h2>
-    <div className="dg-card-section">
-        <div className="dg-card dg-card1 hvr-grow-shadow">
-            <div className="dg-card__title">Модульное оборудование</div>
+<div class="directory-groups__container">
+    <h2 class="directory-groups__title"> <span class="yellow-diamond"></span> Категории интернет-магазина</h2>
+    <div class="dg-card-section">
+        <div class="dg-card dg-card1 hvr-grow-shadow">
+            <div class="dg-card__title">Модульное оборудование</div>
         </div>
-        <div className="dg-card__groop">
-            <div className="dg-card dg-card2 hvr-grow-shadow">
-                <div className="dg-card__title">Блоки питания</div>
+        <div class="dg-card__groop">
+            <div class="dg-card dg-card2 hvr-grow-shadow">
+                <div class="dg-card__title">Блоки питания</div>
             </div>
-            <div className="dg-card dg-card3 hvr-grow-shadow">
-                <div className="dg-card__title">Средства монтажа</div>
+            <div class="dg-card dg-card3 hvr-grow-shadow">
+                <div class="dg-card__title">Средства монтажа</div>
             </div>
         </div>
-        <div className="dg-card dg-card4 hvr-grow-shadow">
-            <div className="dg-card__title dg-card__title_card4">Силовые автоматические выключатели</div>
+        <div class="dg-card dg-card4 hvr-grow-shadow">
+            <div class="dg-card__title dg-card__title_card4">Силовые автоматические выключатели</div>
         </div>
-        <div className="dg-card__groop">
-            <div className="dg-card dg-card5 hvr-grow-shadow">
-                <div className="dg-card__title dg-card__title_dg-card5">Программируемые контроллеры</div>
+        <div class="dg-card__groop">
+            <div class="dg-card dg-card5 hvr-grow-shadow">
+                <div class="dg-card__title dg-card__title_dg-card5">Программируемые контроллеры</div>
             </div>
-            <div className="dg-card__groop">
-                <div className="dg-card dg-card6 hvr-grow-shadow">
-                    <div className="dg-card__title dg-card__title_dg-card5">Индикация</div>
+            <div class="dg-card__groop flex">
+                <div class="dg-card dg-card6 hvr-grow-shadow">
+                    <div class="dg-card__title dg-card__title_dg-card5">Индикация</div>
                 </div>
-                <div className="dg-card dg-card7 hvr-grow-shadow">
-                    <div className="dg-card__title dg-card__title_dg-card5">Модули расширения</div>
+                <div class="dg-card dg-card7 hvr-grow-shadow">
+                    <div class="dg-card__title dg-card__title_dg-card5">Модули расширения</div>
                 </div>
             </div>
         </div>

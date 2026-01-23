52ce975e9435214c69676008fddc721ef436b78a
master <gubinvs@gmail.com>
2026-01-23 20:31:58 +0300

    s

--- src/php/modules/footer.php
+++ src/php/modules/footer.php
@@ -1,7 +1,7 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 ?>
 <footer class="footer">
     <div class="container footer__container">
